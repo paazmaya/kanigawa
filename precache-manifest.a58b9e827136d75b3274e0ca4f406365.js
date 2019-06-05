b0a37f4cea4c9860ba13aea529e524b7f0dde6c2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-05 05:26:15 +0000

    Deploy paazmaya/kanigawa to github.com/paazmaya/kanigawa.git:gh-pages

---
+++ precache-manifest.a58b9e827136d75b3274e0ca4f406365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fad711ba44f1e773c9e93b3b0fa8fa9",
+    "url": "/paazmaya/kanigawa/index.html"
+  },
+  {
+    "revision": "a1360a651fd2e4a91d15",
+    "url": "/paazmaya/kanigawa/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "fed6a1d7e78c079ac41b",
+    "url": "/paazmaya/kanigawa/static/css/main.a6c61fb5.chunk.css"
+  },
+  {
+    "revision": "a1360a651fd2e4a91d15",
+    "url": "/paazmaya/kanigawa/static/js/2.1bb743b4.chunk.js"
+  },
+  {
+    "revision": "fed6a1d7e78c079ac41b",
+    "url": "/paazmaya/kanigawa/static/js/main.9880b3b1.chunk.js"
+  },
+  {
+    "revision": "a7c49ebf8d00ded04fff",
+    "url": "/paazmaya/kanigawa/static/js/runtime~main.0ac54fff.js"
+  }
+]);
